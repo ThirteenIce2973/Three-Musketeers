f33b27f518839d035152b13b80167db01828eca6
ThirteenIce2973 <2713281947@qq.com>
2025-03-22 13:51:06 +0800

    add new files

---
+++ js/loading.js
@@ -0,0 +1,27 @@
+var i = 0;
+function doit(){
+    i = i + 1;
+    var o = document.getElementById('s1');
+    o.innerHTML = i.toString();
+    if(i<100) setTimeout('doit()',50);
+}
+doit();
+
+//加载完成后执行
+window.addEventListener(
+    'load',
+    function () {
+        //载入动画
+        $('#loading-box').attr('class', 'loaded')
+        $('#bg').css(
+            'cssText',
+            'transform: scale(1);filter: blur(0px);transition: ease 1s;'
+        )
+        $('.cover').css('cssText', 'opacity: 1;transition: ease 1s;')
+        $('#section').css(
+            'cssText',
+            'transform: scale(1) !important;opacity: 1 !important;filter: blur(0px) !important'
+        )
+    },
+    false
+)
