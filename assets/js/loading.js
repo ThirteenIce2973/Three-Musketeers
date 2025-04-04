032d61d2344470b24fd75f6a4a6cb25c3924b1a4
ThirteenIce2973 <2713281947@qq.com>
2025-04-04 22:50:53 +0800

    Update 2.5.120-ultimate

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,20 +1,21 @@
 //加载完成后执行
-window.addEventListener(
-    'load',
-    function () {
+const img = new Image();
+img.src = 'assets/img/bg/home-bg.jpg'; // 图片路径
+
+    img.addEventListener('load', function() {
         //载入动画
         $('#loading-box').attr('class', 'loaded')
        $('#bg').css(
             'cssText',
             'transform: scale(1);filter: blur(0px);transition: ease 1s;'
         )
-        // $('.cover').css('cssText', 'opacity: 1;transition: ease 1s;')
+       $('.cover').css('cssText', 'opacity: 1;transition: ease 1s;')
 
-        /*  $('#section').css(
+         $('#section').css(
              'cssText',
              'transform: scale(1) !important;opacity: 1 !important;filter: blur(0px) !important'
          )
-         */
+
     },
     false
 )
