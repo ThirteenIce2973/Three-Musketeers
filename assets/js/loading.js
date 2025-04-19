23488eef7593ecb9ff68a3b79713a9de19a6be89
ThirteenIce2973 <2713281947@qq.com>
2025-04-19 20:42:48 +0800

    add new files

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,27 +1,20 @@
-//加载完成后执行
-// 等待指定section加载完成后执行动画
-const checkSection = setInterval(() => {
-    const section = document.querySelector('#home');
-    if (section) {
-        clearInterval(checkSection);
-
-        // 载入动画
-        $('#loading-box').attr('class', 'loaded');
-        $('#bg').css(
-            'cssText',
-            'transform: scale(1);filter: blur(0px);transition: ease 1.5s;'
-        );
-        $('.cover').css('cssText', 'opacity: 1;transition: ease 1.5s;');
-        $('#section').css(
-            'cssText',
-            'transform: scale(1) !important;opacity: 1 !important;filter: blur(0px) !important'
-        );
-    }
-}, 100);
+setTimeout(function() {
+    // 移除加载动画效果
+    $('#loading-box').attr('class', 'loaded');
+    $('#bg').css(
+        'cssText',
+        'transform: scale(1); filter: blur(0px); transition: ease 1s;'
+    );
+    $('.cover').css('cssText', 'opacity: 1; transition: ease 1s;');
+    $('#section').css(
+        'cssText',
+        'transform: scale(1) !important; opacity: 1 !important; filter: blur(0px) !important'
+    );
+}, 3000);
 
 let progress = 0;
 const interval = 50; // 每50ms更新一次
-const duration = 5000; // 总时长5秒
+const duration = 3000; // 总时长5秒
 const step = 100 / (duration/interval); // 每次增加的百分比
 
 const timer = setInterval(() => {
